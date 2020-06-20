b07d6377fabb84928483ae23c4e2fb558d450fba
Otmane Benazzou <obenazzo@student.42.fr>
2020-06-20 13:35:10 +0100

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@
 // Phantombuster configuration {
 "phantombuster command: nodejs"
 "phantombuster package: 5"
-"phantombuster flags: save-folder"
 
 import Buster from "phantombuster"
 import puppeteer from "puppeteer"
-const validator = require('is-my-json-valid')
 const buster = new Buster()
+const validator = require('is-my-json-valid')
 var marmitonValidator = validator({
   required: true,
   type: 'object',
@@ -23,50 +22,9 @@ var marmitonValidator = validator({
     }
   }
 })
-interface Recipe {
-  title: string
-  url: string
-  rating: number
-  totalRatings: number
-}
 interface Arguments {
   searchURL: string
-  pages: number
-}
-async function isRealRecipe(element: puppeteer.ElementHandle): Promise<boolean>{
-    //checking if it's a sponsored recipe
-    if (await element.$('.recipe-card__sponsored')){
-      return (false)
-    }
-    //checking if it's a recipe and not an album
-    else if (await element.$('.recipe-card__add-to-notebook')){
-      return (true)
-    }
-    else {
-      return (false)
-    }
-}
-async function getRecipes(page: puppeteer.Page, pagesLeftToScrape: number): Promise<Array<Recipe>> {
-  await page.waitForSelector('.recipe-results')
-  var recipes: Array<puppeteer.ElementHandle> = await page.$$('.recipe-card')
-  var recipesData: Array<Recipe> = []
-  for (var i in recipes) {
-    if (await isRealRecipe(recipes[i])) {
-      var recipeData: Recipe = {} as Recipe
-      recipeData.title = await recipes[i].$eval('.recipe-card__title', (element: any) => element.textContent)
-      recipeData.url = await recipes[i].$eval('.recipe-card-link', (element: any) => element.href)
-      recipeData.rating = await recipes[i].$eval('.recipe-card__rating__value', (element: any) => parseFloat(element.textContent))
-      recipeData.totalRatings = await recipes[i].$eval('.mrtn-font-discret', (element: any) => parseInt(element.textContent.match(/[0-9]./)))
-      recipesData.push(recipeData)
-    }
-  }
-  if (pagesLeftToScrape > 1 && (await page.$('.af-pagination .selected + li'))) {
-    //click on next page
-    page.click('.af-pagination .selected + li')
-    await page.waitForNavigation()
-    recipesData = recipesData.concat(await getRecipes(page, pagesLeftToScrape - 1))
-  }
-  return (recipesData)
+  pages?: number
 }
 if (marmitonValidator(buster.argument) === false) {
   throw new Error('Invalid arguments')
@@ -86,3 +44,44 @@ else {
     await browser.close()
     process.exit()
 })()
+interface Recipe {
+  title: string
+  url: string
+  rating: number
+  totalRatings: number
+}
+async function getRecipes(page: puppeteer.Page, pagesLeftToScrape: number): Promise<Array<Recipe>> {
+  await page.waitForSelector('.recipe-results')
+  var recipesElements: Array<puppeteer.ElementHandle> = await page.$$('.recipe-card')
+  var recipesData: Array<Recipe> = []
+  for (var i in recipesElements) {
+    if (await isRealRecipe(recipesElements[i])) {
+      var recipeData: Recipe = {} as Recipe
+      recipeData.title = await recipesElements[i].$eval('.recipe-card__title', (element: any) => element.textContent)
+      recipeData.url = await recipesElements[i].$eval('.recipe-card-link', (element: any) => element.href)
+      recipeData.rating = await recipesElements[i].$eval('.recipe-card__rating__value', (element: any) => parseFloat(element.textContent))
+      recipeData.totalRatings = await recipesElements[i].$eval('.mrtn-font-discret', (element: any) => parseInt(element.textContent.match(/[0-9]./)))
+      recipesData.push(recipeData)
+    }
+  }
+  if (pagesLeftToScrape > 1 && (await page.$('.af-pagination .selected + li'))) {
+    //click on next page
+    page.click('.af-pagination .selected + li')
+    await page.waitForNavigation()
+    recipesData = recipesData.concat(await getRecipes(page, pagesLeftToScrape - 1))
+  }
+  return (recipesData)
+}
+async function isRealRecipe(element: puppeteer.ElementHandle): Promise<boolean>{
+    //checking if it's a sponsored recipe
+    if (await element.$('.recipe-card__sponsored')){
+      return (false)
+    }
+    //checking if it's a recipe and not an album
+    else if (await element.$('.recipe-card__add-to-notebook')){
+      return (true)
+    }
+    else {
+      return (false)
+    }
+}
